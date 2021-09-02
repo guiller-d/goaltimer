52d94dfe0ea9378f9855b635a2bf03f4671328c9
Guiller Dalit <guillerdalit@gmail.com>
2021-09-01 19:47:08 -0700

    folder update

--- app/api/api.js
+++
@@ -1,7 +0,0 @@
-import { create } from 'apisauce';
-
-const baseURL = create({ baseURL: 'http://192.168.1.33:8080/', method: "get", headers: { Accept: 'application/json' } });
-
-export default {
-    baseURL
-}
---
+++ goaltimer-frontend/app/api/api.js
@@ -0,0 +1,7 @@
+import { create } from 'apisauce';
+
+const baseURL = create({ baseURL: 'http://:8080/', method: "get", headers: { Accept: 'application/json' } });
+
+export default {
+    baseURL
+}
