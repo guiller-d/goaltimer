35e230c759f99408ba0fc601c7f4d465c2d22b56
Saima Yunus <saimayunus925@gmail.com>
2021-11-21 14:55:31 -0800

    created ChallengeTime.java for upcoming challenge timer

---
+++ goaltimer-backend/src/main/java/com/example/springrest/model/ChallengeTime.java
@@ -0,0 +1,68 @@
+
+package com.example.springrest.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class ChallengeTime {
+    private @Id @GeneratedValue Long id;
+    @Column(nullable = true)
+    private String time;
+    @Column(nullable = true)
+    private String date;
+    @Column(nullable = true)
+    private String challengeHashID;
+    @Column(nullable = true)
+    private String userHashID;
+    @Column(nullable = true)
+    private String challengeName;
+
+    public ChallengeTime() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getTime() {
+        return this.time;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public String getChallengeHashID() {
+        return this.challengeHashID;
+    }
+
+    public String getUserHashID() {
+        return this.userHashID;
+    }
+    public String getChallengeName() {
+        return this.challengeName;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setChallengeHashID(String challengeHashID) {
+        this.challengeHashID = challengeHashID;
+    }
+
+    public void setChallengeName(String challengeName) {
+        this.challengeName = challengeName;
+    }
+}
