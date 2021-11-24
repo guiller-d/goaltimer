271583b04213b441e28325b5add3319842f11c60
Guiller Dalit <guillerdalit@gmail.com>
2021-11-23 21:37:48 -0800

    merging bin

--- goaltimer-backend/src/test/java/com/example/springrest/SpringRestApplicationTests.java
+++ goaltimer-backend/src/test/java/com/example/springrest/SpringRestApplicationTests.java
@@ -2,12 +2,20 @@ package com.example.springrest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 class SpringRestApplicationTests {
 
+	private RestTemplate restTemplate = new RestTemplate();
+
 	@Test
 	void contextLoads() {
+		ResponseEntity<String> responseDtoResponse = 
+		restTemplate.getForEntity("http://www.google.com", String.class);
+
+		assertThat(responseDtoResponse.getStatusCode().value()).isEqualTo(200);	
 	}
 
 }
